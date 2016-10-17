4588a1d6460db39ca4f07daca94f326efe6ea9a2
anskarthik <anskarthik93@gmail.com>
2016-10-17 22:55:15 +0530

    Learning circles and lines identification, Matlab

--- sample.m
+++ sample.m
@@ -1,3 +1,16 @@
 I_org = imread('board.jpg');
-I = imresize(I_org, 0.4);
-imshow(I)
+I = imresize(rgb2gray(I_org), 0.4);
+BW = edge(I,'canny');
+imshow(BW);
+
+figure(1), imshow(I)
+
+figure(2)
+gray_image = rgb2gray(I);
+imshow(gray_image);
+d = imdistline;
+
+[centers, radii] = imfindcircles(I,[6 15],'ObjectPolarity','dark',...
+    'Sensitivity',0.9);
+figure(1), imshow(I);
+h = viscircles(centers,radii);
